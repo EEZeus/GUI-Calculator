1852a14eecc7dfe724a7ff2ae86ddb68e7f223a4
Ehsan Espandar <eespandar@gmail.com>
2022-12-15 23:39:42 +0330

    js-logic implementing -turning string operators into arithmetic- incomplete v1.0

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const numbers = document.querySelectorAll('.number')
+const globalBtns = document.querySelectorAll('.global')
 
 let output = ''
+let result = document.querySelector('.result')
 
-const sqrt = document.querySelector('#sqrt')
-const lp = document.querySelector('#lp')
-const rp = document.querySelector('#rp')
-const mod = document.querySelector('#mod')
-const dev = document.querySelector('#dev')
-const X = document.querySelector('#X')
-const mines = document.querySelector('#mines')
-const dot = document.querySelector('#dot')
-const plus = document.querySelector('#plus')
 const equal = document.querySelector('#eq')
 const del = document.querySelector('#Del')
 
+const operators={
+    '+':(x,y)=>x+y,
+    '.':(x,y)=>x*y,
+    '-':(x,y)=>x-y,
+    '/':(x,y)=>x/y,
+    '%':(x,y)=>x%y,
+    'sqrt':(x)=>sqrt(x)
+}
+
+for(let i = 0 ;i<globalBtns.length;i++){
+
+    globalBtns[i].addEventListener('click',function(e){
+        if(globalBtns[i].getAttribute('class').includes('number')){
+            output+=globalBtns[i].defaultValue;
+        }else{
+            output+=' '+globalBtns[i].defaultValue+' ';
+        }
+        result.value = output;
+    })
+}
+
+del.addEventListener('click',function(e){
+    output = output.substring(0,output.length-1)
+    result.value = output;
+})
+
+equal.addEventListener('click',function(e){
+   let expArr = output.split(' ')
+   
+})
 
