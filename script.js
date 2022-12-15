8773f854cfbe1c4bd5b37cde07c10d3a3c8f0617
Ehsan Espandar <eespandar@gmail.com>
2022-12-15 22:33:39 +0330

    HTML and CSS done. js starter

---
+++ script.js
@@ -0,0 +1,17 @@
+const numbers = document.querySelectorAll('.number')
+
+let output = ''
+
+const sqrt = document.querySelector('#sqrt')
+const lp = document.querySelector('#lp')
+const rp = document.querySelector('#rp')
+const mod = document.querySelector('#mod')
+const dev = document.querySelector('#dev')
+const X = document.querySelector('#X')
+const mines = document.querySelector('#mines')
+const dot = document.querySelector('#dot')
+const plus = document.querySelector('#plus')
+const equal = document.querySelector('#eq')
+const del = document.querySelector('#Del')
+
+
