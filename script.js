3aee9b2ea8162c2f9d4d11a94643349875d7b102
Ehsan Espandar <eespandar@gmail.com>
2022-12-17 13:05:12 +0330

    Class based calculator starter (only coments)-v2.0

--- script.js
+++ script.js
@@ -1,63 +1,19 @@
-const globalBtns = document.querySelectorAll('.global')
+//class based calculator implement
+/*
+UI elements 
 
-let output = ''
-let result = document.querySelector('.result')
+1.number buttons
+2.operation buttons
+3.equals button
+4.delete button
+5.operands text element (if needed)
 
-const equal = document.querySelector('#eq')
-const del = document.querySelector('#Del')
+methods:
+1.delete
+2.appendNumber
+3.chooseOperation
+4.compute
+5.update display
 
-const operators = {
-    '+': (x, y) => x + y,
-    '.': (x, y) => x * y,
-    '-': (x, y) => x - y,
-    '/': (x, y) => x / y,
-    '%': (x, y) => x % y,
-    'sqrt': (x) => Math.sqrt(x)
-}
-
-for (let i = 0; i < globalBtns.length; i++) {
-
-    globalBtns[i].addEventListener('click', function (e) {
-        if (globalBtns[i].getAttribute('class').includes('number')) {
-            output += globalBtns[i].defaultValue;
-        } else {
-            output += ' ' + globalBtns[i].defaultValue + ' ';
-        }
-        result.value = output;
-    })
-}
-
-del.addEventListener('click', function (e) {
-    output = output.substring(0, output.length - 1)
-    result.value = output;
-})
-
-equal.addEventListener('click', function (e) {
-    let expArr = output.split(' ')
-    while (expArr.includes('sqrt')) {
-        let opIndex = expArr.findIndex(element => element === 'sqrt')
-        let result = operators.sqrt(expArr[opIndex + 1])
-        let newArr = []
-        for (let i = 0; i < expArr.length; i++) {
-            if (i === opIndex) {
-                newArr.push(result)
-            }
-            else if (i === opIndex + 1) {
-                continue
-            }
-            else {
-                newArr.push(expArr[i])
-            }
-        }
-        let tempIndex = expArr.length
-        for (let i = 0 ; i<tempIndex;i++) {
-            expArr.pop()
-        }
-
-        for (let i = 0; i < newArr.length; i++) {
-            expArr.push(newArr[i])
-        }
-    }
-    console.log(expArr)
-})
+*/
 
